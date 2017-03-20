d09a91fe9dac2c7d1baad058cf096d6014f9de78
shaojiejie <jiejieshao@outlook.com>
2017-03-20 22:20:04 +0800

    clearn

---
+++ learnc/intro.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**我的第一个c语言
+   c语言实例
+   操作系统
+   语言编译器
+   汇编器
+   文本编辑器
+   打印机
+   驱动
+   现代程序
+   数据库
+   语言解释器
+   实体工具
+  
+   c编译器 GCC gcc -v
+**/
+int main()
+{
+	printf("hello,world\n");
+	return 0;
+}
